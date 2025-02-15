ab542e1f12507c59307df61298a8f039fc18b431
cbriggs8706 <cameronbriggs8706@gmail.com>
2025-02-15 12:05:46 -0700

    wire up articles

--- blog/blog.js
+++ blog/blog.js
@@ -9,7 +9,7 @@ const articles = [
 		imgAlt: 'Book cover for Septimus Heap 1',
 		ages: '10-14',
 		genre: 'Fantasy',
-		stars: '****'
+		stars: '****',
 	},
 	{
 		id: 2,
@@ -22,6 +22,44 @@ const articles = [
 		imgAlt: 'Book cover for Magnus Chase 1',
 		ages: '12-16',
 		genre: 'Fantasy',
-		stars: '⭐⭐⭐⭐'
-	}
+		stars: '⭐⭐⭐⭐',
+	},
+	{
+		id: 3,
+		title: 'Belgariad Book One: Pawn of Prophecy',
+		date: 'Feb 12, 2022',
+		description:
+			"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg',
+		imgAlt: 'Book cover for Pawn of Prophecy',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐⭐',
+	},
 ]
+
+function articleTemplate(article) {
+	return `<div class="article-grid">
+							<div class="details">
+									<p>${article.date}</p>
+									<p>${article.ages}</p>
+									<p>${article.genre}</p>
+									<p>${article.stars}</p>
+							</div>
+							<div class="content">
+									<h2>${article.title}</h2>
+									<img src="${article.imgSrc}" alt="${article.imgAlt}">
+									<p>${article.description}</p>
+							</div>
+					</div>`
+}
+const articlesHtml = articles.map(articleTemplate)
+
+document.querySelector('#maincontent').innerHTML = `
+  <div id="articles-container">
+    ${articlesHtml.join('')}
+  </div>
+  <div id="filters">
+    <p>Filter form will go here</p>
+  </div>
+`
